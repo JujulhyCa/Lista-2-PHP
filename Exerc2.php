ee865cc0730a0e79146b7018690c3b3bca0d6e7a
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 19:49:57 -0300

    Create Exerc2.php
    2. Entre com 10 números e armazene em um vetor. Ao final o programa deverá mostrar:
    a. quantos negativos foram digitados;
    b. quantos positivos foram digitados;
    c. quantos pares e ímpares.

---
+++ Exerc2.php
@@ -0,0 +1,17 @@
+<!-- 2. Entre com 10 números e armazene em um vetor. Ao final o programa deverá 
+mostrar: 
+a. quantos negativos foram digitados; 
+b. quantos positivos foram digitados; 
+c. quantos pares e ímpares.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
