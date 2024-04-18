10d7b6c4627f80e9f4fe44a635eb78c04f5b906f
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 20:13:55 -0300

    Create Exerc4.php
    4. Crie um vetor que armazene o nome de todos os meses do ano. Peça ao usuário que digite um número e ele informe qual o nome do mês correspondente.

---
+++ Exerc4.php
@@ -0,0 +1,14 @@
+<!--4. Crie um vetor que armazene o nome de todos os meses do ano. Peça ao usuário 
+que digite um número e ele informe qual o nome do mês correspondente. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
