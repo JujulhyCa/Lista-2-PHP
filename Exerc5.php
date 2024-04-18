a10035ecb406c5d3bffd693d8f852b5288b64fc7
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 20:24:54 -0300

    Create Exerc5.php
    5. Faça um programa que leia 20 números inteiros. Crie, a seguir, um vetor resultante que contenha todos os números primos do vetor digitado. Mostre os valores do vetor resultante.

---
+++ Exerc5.php
@@ -0,0 +1,15 @@
+<!-- 5. Faça um programa que leia 20 números inteiros. Crie, a seguir, um vetor 
+resultante que contenha todos os números primos do vetor digitado. Mostre os 
+valores do vetor resultante.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
