248728736c5427c4a35651b9f894c90029767da6
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 19:37:04 -0300

    Create Exerc1.php
    1. Entre com os dados de 10 alunos de uma classe, recebendo as informações como nome e uma nota do aluno. Armazene estes dados em um mapa ordenado. Ao final do programa mostrar a média de nota da classe, e o nome do aluno que obteve maior nota.

---
+++ Exerc1.php
@@ -0,0 +1,16 @@
+<!-- 1. Entre com os dados de 10 alunos de uma classe, recebendo as informações como 
+nome e uma nota do aluno. Armazene estes dados em um mapa ordenado. Ao 
+final do programa mostrar a média de nota da classe, e o nome do aluno que 
+obteve maior nota. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
