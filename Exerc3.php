2ac64722ab10388bbdf6dcb1240dbd2a35caee79
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 19:59:48 -0300

    Create Exerc3.php
    3. Digite 10 valores numéricos e armazene em um mapa ordenado. Em seguida, solicite ao usuário um número para multiplicar todos os elementos do vetor. O programa deverá exibir o resultado da multiplicação do número dado pelo
    usuário em todos os elementos armazenados.

---
+++ Exerc3.php
@@ -0,0 +1,16 @@
+<!-- 3. Digite 10 valores numéricos e armazene em um mapa ordenado. Em seguida, 
+solicite ao usuário um número para multiplicar todos os elementos do vetor. O 
+programa deverá exibir o resultado da multiplicação do número dado pelo 
+usuário em todos os elementos armazenados.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
