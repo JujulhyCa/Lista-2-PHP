f6402ee64160439dadb7f71b2a3f87a8f271569a
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 21:00:55 -0300

    Create Exerc6.php
    6. Faça um programa que receba o nome de 5 produtos e seus respectivos preços, calcule e mostre:
    a. A quantidade de produtos com preço inferior a R$50,00.
    b. O nome dos produtos com preço entre R$50,00 e R$100,00.
    c. A média dos preços dos produtos com preço superior a R$100,00.

---
+++ Exerc6.php
@@ -0,0 +1,17 @@
+<!-- 6. Faça um programa que receba o nome de 5 produtos e seus respectivos preços, 
+calcule e mostre: 
+a. A quantidade de produtos com preço inferior a R$50,00. 
+b. O nome dos produtos com preço entre R$50,00 e R$100,00. 
+c. A média dos preços dos produtos com preço superior a R$100,00. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
