14d2e7c21291f5f05fe39b023ac539959dfd2fe6
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 21:03:07 -0300

    Update Exerc6.php

--- Exerc6.php
+++ Exerc6.php
@@ -5,13 +5,12 @@ b. O nome dos produtos com preço entre R$50,00 e R$100,00.
 c. A média dos preços dos produtos com preço superior a R$100,00. -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Análise de Produtos</title>
 </head>
 <body>
-    
-</body>
-</html>
+    <h2>Análise de Produtos</h2>
+    <form action="" method="post">
