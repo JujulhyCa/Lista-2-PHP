41f76eb49da5bf588e79265c091295c8b4e65318
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 21:08:42 -0300

    Create Exerc7.php
    7. Crie um programa em PHP que receba os seguintes dados de 10 alunos: nome, nota1 e nota2. Armazene esses dados em um mapa ordenado que contenha também o armazenamento da média de nota do aluno. Seu programa deve
    imprimir a lista dos aprovados com as médias finais e outra lista dos reprovados sem exibir o valor da média

---
+++ Exerc7.php
@@ -0,0 +1,17 @@
+<!-- 7. Crie um programa em PHP que receba os seguintes dados de 10 alunos: nome, 
+nota1 e nota2. Armazene esses dados em um mapa ordenado que contenha 
+também o armazenamento da média de nota do aluno. Seu programa deve 
+imprimir a lista dos aprovados com as médias finais e outra lista dos reprovados 
+sem exibir o valor da média. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
